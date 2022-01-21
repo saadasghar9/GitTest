bd4d645c4e82d1a91f28b5d31be0de4ed77dab1f
saad asghar <saadasghar990@gmail.com>
2022-01-22 03:32:57 +0500

    move plz

---
+++ project/pyu.py
@@ -0,0 +1,10 @@
+#working will now be done on the next projecct
+
+from pydoc import doc
+
+
+project = int(input("what the number of project"))
+
+if project > 1:
+    for i in range(project):
+        print("move to next project")
